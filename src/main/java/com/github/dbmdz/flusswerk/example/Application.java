995b7fc81f9b78fe6dd78321d5beb97b1f912a89
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2021-02-02 10:06:26 +0100

    Bump Flusswerk version to 5.0.0-SNAPSHOT

--- src/main/java/com/github/dbmdz/flusswerk/example/Application.java
+++ src/main/java/com/github/dbmdz/flusswerk/example/Application.java
@@ -3,17 +3,19 @@ package com.github.dbmdz.flusswerk.example;
 import com.github.dbmdz.flusswerk.framework.EnableFlusswerk;
 import com.github.dbmdz.flusswerk.framework.FlusswerkApplication;
 import com.github.dbmdz.flusswerk.framework.engine.Engine;
+import java.util.Optional;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableFlusswerk
 public class Application extends FlusswerkApplication {
 
-  public Application(Engine engine) {
+  public Application(Optional<Engine> engine) {
     super(engine);
   }
 
   public static void main(String[] args) {
-    FlusswerkApplication.run(Application.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
